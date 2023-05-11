497e01b2fe87951a3753cbf5d8fe53c6e58ba32f
Yunju <yunjoo1018@gmail.com>
2023-05-11 17:44:08 +0900

    CustomLayout - Composeable Layout() 사용하여 자식 요소 크기, 위치 제어

--- CustomLayout/src/main/java/com/example/customlayout/MainActivity.kt
+++ CustomLayout/src/main/java/com/example/customlayout/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.customlayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.customlayout.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,12 +34,52 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun CascadeLayout(
+    modifier: Modifier = Modifier,
+    spacing: Int = 0,
+    content: @Composable () -> Unit,
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) {
+            measurable, constraints ->      // 측정값, 제약 (제약 조건은 element(요소) 가 가질 수 있는 width, height 에 대한 min,max 범위)
+        var xCoord = 0
+        var yCoord = 0
 
+        layout(constraints.maxWidth, constraints.maxHeight) {    // 제약 범위의 maxWidth, maxHeight
+
+            val placeables = measurable.map { measurable ->     // 자식들 측정 List
+                measurable.measure(constraints)
+            }
+
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = xCoord, y = yCoord)     // 배치할 위치 지정 (x좌표, y좌표)
+                xCoord += placeable.width + spacing                 // 좌표에 간격 추가
+                yCoord += placeable.height + spacing
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(){
+    Box(){
+        CascadeLayout(spacing = 20) {
+            Box(modifier = Modifier.size(60.dp).background(Color.Blue))
+            Box(modifier = Modifier.size(80.dp).background(Color.Red))
+            Box(modifier = Modifier.size(90.dp).background(Color.Cyan))
+            Box(modifier = Modifier.size(50.dp).background(Color.Magenta))
+            Box(modifier = Modifier.size(70.dp).background(Color.Green))
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeTheme {
-
+        MainScreen()
     }
 }
